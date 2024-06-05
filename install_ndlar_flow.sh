e3764fd059c9926bac6e0b9232968c134c248bd9
Matt Kramer <mjkramer@users.noreply.github.com>
2024-06-04 19:34:17 -0700

    Use get_proto_nd_input.sh

--- install_ndlar_flow.sh
+++ install_ndlar_flow.sh
@@ -16,13 +16,6 @@ cd ..
 git clone -b develop https://github.com/larpix/ndlar_flow.git
 cd ndlar_flow
 pip install -e .
-cd ..
-
-datadir=ndlar_flow/data/proto_nd_flow
-
-curl -O --output-dir "$datadir" https://raw.githubusercontent.com/DUNE/larnd-sim/develop/larndsim/pixel_layouts/multi_tile_layout-2.4.16.yaml
-curl -O --output-dir "$datadir" https://raw.githubusercontent.com/DUNE/larnd-sim/develop/larndsim/pixel_layouts/multi_tile_layout-2.5.16.yaml
-curl -O --output-dir "$datadir" https://raw.githubusercontent.com/DUNE/larnd-sim/develop/larndsim/detector_properties/2x2.yaml
-
-cp static/runlist-2x2-mcexample.txt $datadir
-cp static/light_module_desc-0.0.0.yaml $datadir
+cd scripts/proto_nd_scripts
+./get_proto_nd_input.sh
+cd ../../..
