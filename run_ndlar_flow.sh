47662564be87e7108c14834c47af1e6e8a02c1a3
Matt Kramer <mjkramer@users.noreply.github.com>
2024-12-10 12:48:02 -0800

    Update yaml filenames

--- run_ndlar_flow.sh
+++ run_ndlar_flow.sh
@@ -15,18 +15,18 @@ source ndlar_flow.venv/bin/activate
 cd ndlar_flow
 
 # charge workflows
-workflow1='yamls/proto_nd_flow/workflows/charge/charge_event_building.yaml'
-workflow2='yamls/proto_nd_flow/workflows/charge/charge_event_reconstruction.yaml'
-workflow3='yamls/proto_nd_flow/workflows/combined/combined_reconstruction.yaml'
-workflow4='yamls/proto_nd_flow/workflows/charge/prompt_calibration.yaml'
-workflow5='yamls/proto_nd_flow/workflows/charge/final_calibration.yaml'
+workflow1='yamls/proto_nd_flow/workflows/charge/charge_event_building_mc.yaml'
+workflow2='yamls/proto_nd_flow/workflows/charge/charge_event_reconstruction_mc.yaml'
+workflow3='yamls/proto_nd_flow/workflows/combined/combined_reconstruction_mc.yaml'
+workflow4='yamls/proto_nd_flow/workflows/charge/prompt_calibration_mc.yaml'
+workflow5='yamls/proto_nd_flow/workflows/charge/final_calibration_mc.yaml'
 
 # light workflows
 workflow6='yamls/proto_nd_flow/workflows/light/light_event_building_mc.yaml'
-workflow7='yamls/proto_nd_flow/workflows/light/light_event_reconstruction.yaml'
+workflow7='yamls/proto_nd_flow/workflows/light/light_event_reconstruction_mc.yaml'
 
 # charge-light trigger matching
-workflow8='yamls/proto_nd_flow/workflows/charge/charge_light_assoc.yaml'
+workflow8='yamls/proto_nd_flow/workflows/charge/charge_light_assoc_mc.yaml'
 
 h5flow -c $workflow1 $workflow2 $workflow3 $workflow4 $workflow5\
     -i "$inFile" -o "$outFile"
